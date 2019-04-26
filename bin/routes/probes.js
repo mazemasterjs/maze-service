39c2dc1dac7c0e95ad3f7f7b1c8daa811d5015d0
James Dean <kreebog@gmail.com>
2019-04-26 16:21:49 -0400

    removed duplicate routes, fixed CORS config

--- bin/routes/probes.js
+++ bin/routes/probes.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const logger_1 = require("@mazemasterjs/logger");
-exports.probesRouter = express_1.default.Router();
 const Config_1 = __importDefault(require("@mazemasterjs/shared-library/Config"));
+exports.probesRouter = express_1.default.Router();
 const log = logger_1.Logger.getInstance();
 const config = Config_1.default.getInstance();
 const RES_LIVE_TRUE = { probeType: 'liveness', status: 'alive' };
